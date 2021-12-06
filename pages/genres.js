b127bbb092421449f8020944a54b6ed1fdb60c71
Gustavo <gus.inacio@hotmail.com>
2021-12-06 11:31:42 -0300

    impl: funcional genre search

--- pages/genres.js
+++ pages/genres.js
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { apiBase, apiKey } from '../lib/tmdb';
 import Header from '../components/header';
 import styles from '../styles/Home.module.css';
 
-export default function genres({ list }) {
+export default function Genres({ list }) {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async (id) => {
+    const result = await fetch(`${apiBase}/discover/movie?with_genres=${id}&language=pt-BR&api_key=${apiKey}`)
+    const json = await result.json();
+    setMovies(json.results);
+  }
+
+  useEffect(() => {
+    console.log(movies)
+  }, [movies])
   return (
     <div>
       <Header />
@@ -10,10 +22,23 @@ export default function genres({ list }) {
         <ul className={styles.genrelist}>
             {list.map(item => (
               <li className={styles.genrecard} key={ item.id }>
-                <p>{item.name}</p>
+                <p onClick={ () => getMovies(item.id) }>{item.name}</p>
               </li>
             ))}
           </ul>
+        <div>
+        <ul className={styles.movielist}>
+              {movies.length > 0 && movies.map(movie => (
+                <li className={styles.moviecard} key={movie.title}>
+                    <a href={`/movie/${movie.id}`}>
+                      <img width="200" height="300" src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.title} />
+                      <br />
+                      <p>{movie.title}</p>
+                    </a>
+                </li>
+              ))}
+            </ul>
+        </div>
       </main>
     </div>
   )
